34d97d80e7e759f7ca89e610c0ddf2833ea022b7
trishramdeenmonelal <shawn.ramdeen16@gmail.com>
2020-03-07 12:53:59 -0600

    Form complete without uploading pics

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SelectField, TextAreaField
+from wtforms.validators import DataRequired, Email 
+from flask import render_template, request, redirect, url_for, flash
 
 class MyForm(FlaskForm):
     firstname = StringField('First Name', validators=[DataRequired()])
     lastname  = StringField('Last Name', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    gender = StringField('Gender', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    gender = SelectField('Gender', choices=[('', 'Select your CORRECT gender'),('M','Male'),('F','Female')])
     location = StringField('Location', validators=[DataRequired()])
-    biography = StringField('Biography', validators=[DataRequired()])
+    biography = TextAreaField('Biography', validators=[DataRequired()])
     
