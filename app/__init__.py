cc5d6545b295822787c2bf081d03f8e65e586931
trishramdeenmonelal <shawn.ramdeen16@gmail.com>
2020-03-07 12:01:27 -0600

    Minor errors solved

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "secretkey"
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:password123@localhost/project1"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+
+
+app.config.from_object(__name__)
+from app import views
